6d3cd952f7462a0ff14727329d61fb56905dabb9
Muskankumari2002 <86469435+Muskankumari2002@users.noreply.github.com>
2022-01-06 22:42:42 +0530

    Average of two number

---
+++ average.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,c,d,total,avg;
+    cout<<"Input two number :"<<endl;
+    cin>>a>>b;
+    cout<<"Input two number: "<<endl;
+    cin>>c>>d;
+    total=a+b+c+d;
+    avg=total/4;
+    cout<<"The tota of four number: "<<total<<endl;
+    cout<<"The average of four number: "<<avg;
+    return 0;
+
+}
